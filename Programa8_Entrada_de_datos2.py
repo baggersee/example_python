aacd5b3b7be393add277fccb825263c7c1e41445
baggersee <79531778+baggersee@users.noreply.github.com>
2021-02-23 18:29:30 +0100

    Add files via upload

---
+++ Programa8_Entrada_de_datos2.py
@@ -0,0 +1,12 @@
+nombre = input("¿Cuál es tu nombre? ")
+print(" ")
+print("Hola " + nombre + ", vamos a realizar una suma.")
+print(" ")
+
+num_uno = float(input("Introduce el primer numero: "))
+num_dos = float(input("Introduce el segundo numero: "))
+
+resultado = num_uno + num_dos
+
+print(" ")
+print(nombre + ", el resultado de la suma es: ", resultado)
